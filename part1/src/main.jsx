65096d2d2e31e26aaf17588ccc69a2ca463b5083
Alejandro <largoalejo@gmail.com>
2022-03-26 12:05:10 -0500

    teoria parte 1

--- part1/src/main.jsx
+++ part1/src/main.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hellow {props.name}</p>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greeting</h1>
+      <Hello name='Alejandro' />
+      <Hello name='Pepe' />
+      <Hello name='Argemiro' />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
