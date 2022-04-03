3fa5adc0ee6ad9c2d066fea4150c7e5dd92d18ce
Alejandro <largoalejo@gmail.com>
2022-04-03 16:13:18 -0500

    Ejercicio 2.8-9 phoneBook

--- part2/Ejercicios/phoneBook/src/App.jsx
+++ part2/Ejercicios/phoneBook/src/App.jsx
@@ -1,36 +1,74 @@
 import React, { useState } from 'react'
 
 function App () {
-  const [persons, setPersons] = useState([{ name: 'jhon doe' }])
+  const [persons, setPersons] = useState([
+    { name: 'Jhon doe', number: '301' },
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filterValue, setFilterValue] = useState('')
 
   const changeNewName = (e) => {
     setNewName(e.target.value)
   }
+
+  const changeNewNumber = (e) => {
+    setNewNumber(e.target.value)
+  }
+
+  const filterShownData = (e) => {
+    setFilterValue(e.target.value)
+  }
+
   const addPerson = (e) => {
     e.preventDefault()
     if (
       persons.find((person) => {
-        return person.name === newName
+        return person.number === newNumber
       })
     ) {
-      alert(`${newName} is already added to phonebook`)
+      alert(`${newNumber} is already added to phonebook`)
       return
     }
-    setPersons([...persons, { name: newName }])
+    setPersons([...persons, { name: newName, number: newNumber }])
+    setNewName('')
     setNewName('')
   }
+
+  const personsToShow =
+    filterValue !== ''
+      ? persons.filter((person) =>
+        person.name.toLowerCase().includes(filterValue.toLowerCase())
+      )
+      : persons
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with
+        <input type='text' onChange={filterShownData} />
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={addPerson}>
         <div>
-          name:{' '}
+          name:
           <input
             value={newName}
             onChange={changeNewName}
             placeholder='new contact'
+          />
+        </div>
+        <div>
+          number:
+          <input
+            value={newNumber}
+            onChange={changeNewNumber}
+            placeholder='new number'
           />
         </div>
         <div>
@@ -39,8 +77,12 @@ function App () {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => {
-          return <li key={person.name}>{person.name}</li>
+        {personsToShow.map((person) => {
+          return (
+            <li key={person.number}>
+              {person.name} -- {person.number}
+            </li>
+          )
         })}
       </ul>
     </div>
