ae0435c68a3152ffd5016a6a7522d612147d76a2
Alejandro <largoalejo@gmail.com>
2022-04-10 21:43:59 -0500

    Parte 3 recibiendo informacion

--- part3/part3-a/index.js
+++ part3/part3-a/index.js
@@ -1,4 +1,6 @@
-const http = require('http')
+const express = require('express')
+const app = express()
+app.use(express.json())
 
 let notes = [
   {
@@ -21,11 +23,45 @@ let notes = [
   },
 ]
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'application/json' })
-  response.end(JSON.stringify(notes))
+app.get('/', (request, response) => {
+  response.send('<h1>Hello world!!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find((note) => note.id === id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).send('<h1>Error not found (401)</h1>')
+    // response.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter((note) => note.id !== id)
+  response.status(204).end()
+})
+
+app.post('/api/notes', (request, response) => {
+  const { content, important = false } = request.body
+  const date = new Date().toISOString()
+  const id = notes.length + 1
+  if (!content) {
+    return response.status(400).json({ error: 'Content missing' })
+  }
+
+  const note = { id, content, date, important }
+  notes = [...notes, note]
+  response.json(note)
 })
 
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server is running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
